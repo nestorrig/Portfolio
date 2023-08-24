27dc6f34bd546693c752020f48dae0ea120c88c6
nestorrig <neriga10@hotmail.com>
2023-08-24 17:46:53 -0600

    v2.21: modifie menu mobile, add interactive menu with js

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const interactiveElements = document.querySelectorAll(".interactive");
 const switchElements = document.querySelectorAll(".switch-theme");
+const main = document.querySelector('main')
 console.log(switchElements);
 
-const iconMenu = document.getElementById('Menu-icon')
-const mobileMenu = document.getElementById('Mobile-menu')
-const mobileToggleSwitch = document.getElementById('toggle-mobile')
-const desktopToggleSwitch = document.getElementById('toggle-desktop')
+const menuButtons = document.getElementById('MenuButtons')
+const openMenu = document.getElementById('OpenMenu')
+const closeMenu = document.getElementById('CloseMenu')
+const mobileMenu = document.getElementById('MobileMenu')
+// const mobileToggleSwitch = document.getElementById('toggle-mobile')
+// const desktopToggleSwitch = document.getElementById('toggle-desktop')
 
-iconMenu.addEventListener("click", ()=> {blockOrNone(mobileMenu)});
-mobileToggleSwitch.addEventListener("click", ()=> {switchThemeMobile()});
-desktopToggleSwitch.addEventListener("click", ()=> {switchThemeDesktop()});
+console.log({
+    openMenu, closeMenu, mobileMenu
+});
 
-function blockOrNone(element) {//https://platzi.com/comentario/3856000/
+menuButtons.addEventListener("click", ()=> {blockOrNone(mobileMenu)});
+menuButtons.addEventListener("click", ()=> switchButtons(openMenu, closeMenu));
+
+function switchButtons(btn1,  btn2) {
+    if (btn1.classList.contains("inactive") === true) {
+        btn1.classList.remove("inactive")
+        btn2.classList.add("inactive")
+        main.style.filter = 'none'
+    } else {
+        btn1.classList.add("inactive")
+        btn2.classList.remove("inactive")
+        main.style.filter = 'brightness(0.5)'
+    }
+}
+
+
+function blockOrNone(element) {
     if (element.classList.contains("inactive") === true) {
-        displayNoneAll()
         element.classList.remove("inactive")
     } else {
-        //displayNoneAll()
         element.classList.add("inactive")
     }
 }
-function displayNoneAll() {
-    interactiveElements.forEach(function(interactiveElement){
-        if (interactiveElement.classList.contains("inactive") === false) {
-            interactiveElement.classList.add("inactive");
-        }
-    })
-}
+// function displayNoneAll() {
+//     interactiveElements.forEach(function(interactiveElement){
+//         if (interactiveElement.classList.contains("inactive") === false) {
+//             interactiveElement.classList.add("inactive");
+//         }
+//     })
+// }
 function switchThemeDesktop() {
     if (desktopToggleSwitch.checked) {
         switchElements.forEach(element => {
