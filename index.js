eb9135412d41f9acd1f36042c3e7b8c83b2c95b8
nestorrig <neriga10@hotmail.com>
2023-08-25 00:56:49 -0600

    v2.25: modifie js file, add animations to mobile menu & menu icons

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
-const interactiveElements = document.querySelectorAll(".interactive");
 const switchElements = document.querySelectorAll(".switch-theme");
 const main = document.querySelector('main')
-console.log(switchElements);
+const navItems = document.getElementsByClassName('header__nav__item--mobile')
 
 const menuButtons = document.getElementById('MenuButtons')
 const openMenu = document.getElementById('OpenMenu')
 const closeMenu = document.getElementById('CloseMenu')
 const mobileMenu = document.getElementById('MobileMenu')
-// const mobileToggleSwitch = document.getElementById('toggle-mobile')
-// const desktopToggleSwitch = document.getElementById('toggle-desktop')
 
-console.log({
-    openMenu, closeMenu, mobileMenu
-});
-
-menuButtons.addEventListener("click", ()=> {blockOrNone(mobileMenu)});
-menuButtons.addEventListener("click", ()=> switchButtons(openMenu, closeMenu));
-
-function switchButtons(btn1,  btn2) {
-    if (btn1.classList.contains("inactive") === true) {
-        btn1.classList.remove("inactive")
-        btn2.classList.add("inactive")
-        main.style.filter = 'none'
-    } else {
-        btn1.classList.add("inactive")
-        btn2.classList.remove("inactive")
-        main.style.filter = 'brightness(0.5)'
-    }
+for (const item of navItems) {
+    item.addEventListener("click",showOrHideMenu)
+    switchButtons(openMenu, closeMenu)
 }
+menuButtons.addEventListener("click",showOrHideMenu);
 
-
-function blockOrNone(element) {
-    if (element.classList.contains("inactive") === true) {
-        element.classList.remove("inactive")
-    } else {
-        element.classList.add("inactive")
-    }
+function switchButtons() {
+    openMenu.classList.toggle("inactive")
+    closeMenu.classList.toggle("inactive")
 }
-// function displayNoneAll() {
-//     interactiveElements.forEach(function(interactiveElement){
-//         if (interactiveElement.classList.contains("inactive") === false) {
-//             interactiveElement.classList.add("inactive");
-//         }
-//     })
-// }
-function switchThemeDesktop() {
-    if (desktopToggleSwitch.checked) {
-        switchElements.forEach(element => {
-            element.classList.add("black-theme")
-        });
+function showOrHideMenu() {
+    const test = mobileMenu.classList.contains("hide-menu")
+    if (test) {
+        mobileMenu.classList.remove("hide-menu")
+        main.classList.add("brightness")
     } else {
-        switchElements.forEach(element => {
-            element.classList.toggle("black-theme")
-        });
+        mobileMenu.classList.add("hide-menu")
+        main.classList.remove("brightness")
     }
-}
-function switchThemeMobile() {
-    if (mobileToggleSwitch.checked) {
-        switchElements.forEach(element => {
-            element.classList.add("black-theme")
-        });
-    } else {
-        switchElements.forEach(element => {
-            element.classList.toggle("black-theme")
-        });
-    }
+    switchButtons()
+    main.addEventListener("click", ()=> {
+        mobileMenu.classList.add("hide-menu")
+        main.classList.remove("brightness")
+        openMenu.classList.remove("inactive")
+        closeMenu.classList.add("inactive")
+    })
 }
