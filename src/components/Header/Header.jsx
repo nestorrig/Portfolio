fad9c1c1e5447bb6e3bf913a533fe101605d2302
Nestor Rios Garcia <neriga10@hotmail.com>
2024-03-08 21:01:07 -0600

    fix: remove excesive animations in footer and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,28 @@
-import { ThemeButton } from "../Buttons/ThemeButton"
+import { ThemeButton } from "../Buttons/ThemeButton";
 
-export function Header () {
-    return (
-        <header className="header">
-                <figure className="navBar__logo navBar__logo--desktop svg__logo">
-                    <img src="./src/assets/logos/logo_letters.svg" alt="Logo"/>
-                </figure>
-                <nav className="navBar navBar--desktop">
-                    <div className="navBar__div">
-                        <a className="navBar__item" href="#About">About me</a>
-                    </div>
-                    <div className="navBar__div">
-                        <a className="navBar__item" href="#Skills">Skills</a>
-                    </div>
-                    <div className="navBar__div">
-                        <a className="navBar__item" href="#Projects">Projects</a>
-                    </div>
-                    <div className="navBar__div">
-                        <a className="navBar__item" href="#Contact">Contact</a>
-                    </div>
-                    <div className="navBar__div">
-                        <a className="navBar__item containCV" href="./src/assets/docs/Nesto_Rios_Garcia-CV.pdf">
-                            <p>CV</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                                <path fill="none" stroke="#fcfcfc" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9 17h6M12 6v7m0 0l3.5-3.5M12 13L8.5 9.5M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </nav>
-                <ThemeButton
-                    device="desktop"
-                />
-            </header>
-    )
+const navLinks = [
+  { label: "About me", link: "#About" },
+  { label: "Skills", link: "#Skills" },
+  { label: "Projects", link: "#Projects" },
+  { label: "Contact", link: "#Contact" },
+  { label: "CV", link: "./src/assets/docs/Nesto_Rios_Garcia-CV.pdf" },
+];
+
+export function Header() {
+
+  return (
+    <header className="header">
+      <figure className="navBar__logo navBar__logo--desktop svg__logo">
+        <img src="./src/assets/logos/logo_letters.svg" alt="Logo" />
+      </figure>
+      <nav className="navBar navBar--desktop">
+        {navLinks.map((link, index) => (
+          <a key={index} className="navBar__item" href={link.link}>
+            {link.label}
+          </a>
+        ))}
+      </nav>
+      <ThemeButton device="desktop" />
+    </header>
+  );
 }
